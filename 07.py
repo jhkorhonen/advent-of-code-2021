9eebd7ab885612a7f491bfff89e1df15db1b2540
Janne H. Korhonen <janne.h.korhonen@gmail.com>
2021-12-07 10:05:25 +0200

    day 7; use math

--- 07.py
+++ 07.py
@@ -1,16 +1,18 @@
 from tools import read
+from math import floor, ceil
+from statistics import mean,median
 
 crabs = [ int(x) for x in read("07.txt")[0].split(",")]
-min_pos = min(crabs)
-max_pos = max(crabs)
+
+def eval(f,z,l):
+    return sum( [ f(abs(x - z)) for x in l] )
 
 # 1
 
-print(min([sum([abs(x-pos) for x in crabs]) for pos in range(min_pos,max_pos+1)]))
+print(eval( lambda n: n, floor(median(crabs)), crabs))
+print(eval( lambda n: n, ceil(median(crabs)), crabs))
 
 # 2
 
-def cost(n):
-    return (n * (n + 1)) / 2
-
-print(min([sum([cost(abs(x-pos)) for x in crabs]) for pos in range(min_pos,max_pos+1)]))
+print(eval( lambda n: n * (n+1)/2, floor(mean(crabs)), crabs))
+print(eval( lambda n: n * (n+1)/2, ceil(mean(crabs)), crabs))
