c458d5c86dad9094e3934a9a1dfac4f3583d98db
Janne H. Korhonen <janne.h.korhonen@gmail.com>
2021-12-07 11:03:56 +0200

    day 7, fix mathematics

--- 07.py
+++ 07.py
@@ -9,10 +9,15 @@ def eval(f,z,l):
 
 # 1
 
-print(eval( lambda n: n, floor(median(crabs)), crabs))
-print(eval( lambda n: n, ceil(median(crabs)), crabs))
+print(eval( lambda n: n, round(median(crabs)), crabs))
 
 # 2
 
-print(eval( lambda n: n * (n+1)/2, floor(mean(crabs)), crabs))
-print(eval( lambda n: n * (n+1)/2, ceil(mean(crabs)), crabs))
+# illegal heuristic solution
+print(eval( lambda n: n * (n+1)/2.0, mean(crabs), crabs))         # individual objectives are not actually quadratic, 
+print(eval( lambda n: n * (n+1)/2.0, floor(mean(crabs)), crabs))  # so mean is not the optimum
+print(eval( lambda n: n * (n+1)/2.0, ceil(mean(crabs)), crabs))   # close enough though  ¯\_(ツ)_/¯
+
+# legal brute-force solution
+print(min( [eval( lambda n: n * (n+1)/2.0, i, crabs) for i in range(min(crabs),max(crabs)+1)] ))
+
