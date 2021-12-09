dd7ec7ff27bd962ba37166d1836df28b8c262e12
Janne H. Korhonen <janne.h.korhonen@gmail.com>
2021-12-09 14:37:47 +0200

    day 9; better flooding

--- 09.py
+++ 09.py
@@ -1,5 +1,5 @@
 from tools import read
-from collections import Counter
+from collections import Counter, deque
 from math import prod
 
 # parsing
@@ -7,7 +7,6 @@ from math import prod
 data = read("09.txt")
 
 heights = [[int(c) for c in line.strip()] for line in data]
-print(heights)
 
 lx = len(heights[0])
 ly = len(heights)
@@ -38,25 +37,32 @@ print(total_risk)
 low_points = 0
 basin_map = [[0 for x in range(lx) ] for y in range(ly)]
 
+queue = deque()
+
+# invariant: if (x,y) is or has been in queue, it has non-zero value in basin_map
+# -1 is ridge line
+# 0 is unvisited
+
 for (x,y) in ((x,y) for x in range(lx) for y in range(ly)):
     if all(heights[x1][y1] > heights[x][y] for (x1,y1) in neighbours(x,y)):
         basin_map[x][y] = low_points + 1
-        print(x,y,basin_map[x][y])
         low_points = low_points + 1
+        queue.append((x,y))
+    if heights[x][y] == 9:
+        basin_map[x][y] = -1
+
+while len(queue) >= 1:
+    x,y = queue.popleft()
+    for (x1,y1) in neighbours(x,y):
+        if basin_map[x1][y1] == 0:
+            basin_map[x1][y1] = basin_map[x][y]
+            queue.append((x1,y1))
 
 for line in basin_map:
-    print("".join(str(x%10) for x in line))
+    line = [str(x%10) if x != -1 else "█" for x in line]
+    print("".join(line))
 
-# naive flooding because lazy
-for i in range(lx+ly):
-    for (x,y) in ((x,y) for x in range(lx) for y in range(ly)):
-        if heights[x][y] < 9:
-            basin_map[x][y] = max([basin_map[x1][y1] for (x1,y1) in neighbours(x,y) if heights[x1][y1] < 9]+ [basin_map[x][y]])
-
-for line in basin_map:
-    print("".join(str(x%10) for x in line))
-
-counts = Counter(x for line in basin_map for x in line if x != 0)
+counts = Counter(x for line in basin_map for x in line if x != -1)
 print(prod( c for n,c in counts.most_common(3) ))
 
 
