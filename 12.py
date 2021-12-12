cc2b424e53221a4379933cc8f2f9520a9696328f
Janne H. Korhonen <janne.h.korhonen@gmail.com>
2021-12-12 17:30:20 +0200

    day 12; mad science solution

--- 12.py
+++ 12.py
@@ -1,5 +1,5 @@
 from tools import read
-from collections import defaultdict
+from collections import defaultdict, Counter
 from itertools import chain
 
 data = read("12.txt")
@@ -12,8 +12,57 @@ n = len(nodes)
 
 def issmall(node):
     return node[0].islower()
+    
+# matrices
 
-class poly:
+class mat:
+    def __init__(self, values, n, ring):
+        self.values = values
+        self.n = n
+        self.ring = ring
+
+    def __add__(A,B):
+        C = mat.zero(A.n, A.ring)
+        for i in range(A.n):
+            for j in range(A.n):
+                C[i,j] = A[i,j] + B[i,j]
+        return C
+
+    def __mul__(A,B):
+        C = mat.zero(A.n,A.ring)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    C[i,j] += A[i,k] * B[k,j]
+        return C
+
+    def __eq__(A,B):
+        for i in range(A.n):
+            for j in range(A.n):
+                if A[i,j] != B[i,j]:
+                    return False
+        return True
+    
+    def __getitem__(self, pos):
+        i,j = pos
+        return self.values[i][j]
+    
+    def __setitem__(self,pos,key):
+        i,j = pos
+        self.values[i][j] = key
+    
+    @classmethod
+    def zero(cls, n, ring):
+        return mat([[ring.zero() for _ in range(n)] for _ in range(n)], n, ring)
+    
+    @classmethod
+    def id(cls, n, ring):
+        return mat([[ring.one() if i == j else ring.zero() for i in range(n)] for j in range(n)], n, ring)
+
+    
+# polynomial ring with non-constant non-multilinear terms discarded
+
+class multilinear:
     def __init__(self, values):
         # values should be a defaultdict indexed with frozensets
         self.values = values
@@ -23,7 +72,7 @@ class poly:
         for k in chain(self.values.keys(), other.values.keys()):
             if self.values[k] + other.values[k] != 0:
                 new_values[k] = self.values[k] + other.values[k]
-        return poly(new_values)
+        return self.__class__(new_values)
     
     def __mul__(self,other):
         # multiply, discard terms that are not multilinear
@@ -32,7 +81,7 @@ class poly:
             for s2, v2 in other.values.items():
                 if s1.isdisjoint(s2) and v1*v2 != 0:
                     new_values[frozenset(s1 | s2)] += v1*v2
-        return poly(new_values)
+        return self.__class__(new_values)
     
     def __eq__(self,other):
         return self.values == other.values
@@ -47,89 +96,84 @@ class poly:
             return " + ".join([ str(v) + "·".join(sorted(k)) for k,v in self.values.items()])
     
     @classmethod
-    def zero(cls,x):
+    def constant(cls,x):
         values = defaultdict(int)
         values[frozenset()] = x
-        return poly(values)
+        return cls(values)
     
     @classmethod
     def mono(cls,variable,x):
         values = defaultdict(int)
         values[frozenset([variable])] = x
-        return poly(values)
-    
-    
-    @classmethod
-    def from_list(cls,l):
-        values = defaultdict(int)
-        for k, v in l:
-            values[frozenset(k)] = v
-        return poly(values)
+        return cls(values)
 
-# matrix operations
-
-class mat:
-    def __init__(self, values):
-        self.values = values
-
-    def __add__(A,B):
-        C = mat.zero()
-        for i in range(n):
-            for j in range(n):
-                C[i,j] = A[i,j] + B[i,j]
-        return C
-
-    def __mul__(A,B):
-        C = mat.zero()
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    C[i,j] += A[i,k] * B[k,j]
-        return C
-
-    def __eq__(A,B):
-        for i in range(n):
-            for j in range(n):
-                if A[i,j] != B[i,j]:
-                    return False
-        return True
-    
-    def __getitem__(self, pos):
-        i,j = pos
-        return self.values[i][j]
-    
-    def __setitem__(self,pos,key):
-        i,j = pos
-        self.values[i][j] = key
-    
     @classmethod
     def zero(cls):
-        return mat([[poly.zero(0) for _ in range(n)] for _ in range(n)])
+        return cls.constant(0)
     
     @classmethod
-    def id(cls):
-        return mat([[poly.zero(1) if i == j else poly.zero(0) for i in range(n)] for j in range(n)])
+    def one(cls):
+        return cls.constant(1)
 
-# 1
 
-I = mat.id()
-A = mat([[poly.zero(0) for _ in range(n)] for _ in range(n)])
-A[0,0] = poly.mono("start",1)
+# I don't know what to call this
+# polynomial ring but terms with at most degree 2 variable allowed
 
-B = mat([[poly.zero(0) for _ in range(n)] for _ in range(n)])
-for edge in edges:
-    v,u = edge
-    B[ni[v],ni[u]] = poly.mono(u,1) if issmall(u) else poly.zero(1)
-    B[ni[u],ni[v]] = poly.mono(v,1) if issmall(v) else poly.zero(1)
+class onedegreetwo(multilinear):
+    def __mul__(self,other):
+        new_values = defaultdict(int)
+        for s1, v1 in self.values.items():
+            for s2, v2 in other.values.items():
+                s_new = s1 + s2
+                
+                if s_new.count("start") >= 2 or s_new.count("end") >= 2 or len(s_new) > len(set(s_new)) + 1:
+                   pass
+                elif v1*v2 != 0:
+                    new_values[tuple(sorted(s_new))] += v1*v2
+        return onedegreetwo(new_values)
 
-current = A * B 
-last = A
-power, summed = B, B
-while not current == last:
-    summed = (power+I) * summed  
-    power = power * power
-    last = current
-    current = A * (summed + I)
-  
-print(current[0,n-1].total())
+    def __str__(self):
+        if len(self.values)  == 0:
+            return "0"
+        else:
+            return " + ".join([ str(v) + "·" + str(k) for k,v in self.values.items()])
+                
+    @classmethod
+    def constant(cls,x):
+        values = defaultdict(int)
+        values[tuple()] = x
+        return cls(values)
+    
+    @classmethod
+    def mono(cls,variable,x):
+        values = defaultdict(int)
+        values[tuple([variable])] = x
+        return cls(values)
+    
+# matrix operations
+
+# 1,2
+
+for ring in [multilinear, onedegreetwo]:
+    I = mat.id(n, ring)
+    A = mat.zero(n, ring)
+    A[0,0] = ring.mono("start",1)
+    
+    B = mat.zero(n, ring)
+    for edge in edges:
+        v,u = edge
+        B[ni[v],ni[u]] = ring.mono(u,1) if issmall(u) else ring.one()
+        B[ni[u],ni[v]] = ring.mono(v,1) if issmall(v) else ring.one()
+    
+    current = A * B 
+    last = A
+    power, summed = B, B
+    i = 0
+    while not current == last:
+        summed = (power+I) * summed  
+        power = power * power
+        last = current
+        current = A * (summed + I)
+      
+    print(current[0,n-1].total())
 
